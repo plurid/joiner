c2fc8ff014860c947fc698660be935f434459591
Jan Cavel <caveljan@gmail.com>
2020-03-24 21:32:13 +0200

    joiner > dev: commands > run

--- packages/joiner/source/commands/run.ts
+++ packages/joiner/source/commands/run.ts
@@ -12,6 +12,32 @@ const runCommand = async (
     if (!configurationData) {
         return;
     }
+
+    const safePackageName = packageName.toLowerCase();
+
+    const {
+        packages,
+    } = configurationData;
+
+    if (safePackageName === 'all') {
+        // TOOD
+        // run command on all packages
+        return;
+    }
+
+    for (const codePackage of packages) {
+        if (typeof codePackage === 'string') {
+            if (codePackage === packageName) {
+                // run command
+                return;
+            }
+            continue;
+        }
+
+        if (codePackage.name === packageName) {
+            // run command;
+        }
+    }
 }
 
 
