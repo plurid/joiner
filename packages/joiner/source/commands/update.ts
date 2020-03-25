640f6e6121ac5a7eada65e3128a54bfcf71c5f52
ly3xqhl8g9 <ly3xqhl8g9@plurid.com>
2020-03-25 08:34:17 +0200

    joiner > dev: commands > update

--- packages/joiner/source/commands/update.ts
+++ packages/joiner/source/commands/update.ts
@@ -1,3 +1,8 @@
+import path from 'path';
+import {
+    execSync,
+} from 'child_process';
+
 import {
     Package,
 } from '../data/interfaces';
@@ -9,6 +14,8 @@ import {
 import {
     resolvePackage,
 } from '../services/logic/packages';
+
+import checkUpdates from 'npm-check-updates';
 
 
 
@@ -32,9 +39,53 @@ const updateCommand = async (
 
 
 const updateLogic = async (
-    configPackage: string | Package,
+    configPackage: Package,
 ) => {
-    console.log(configPackage);
+    const inThePackage = `in the package '${configPackage.name}'`;
+
+    console.log(`\n\tChecking for updates ${inThePackage}...`);
+
+    try {
+        const updatedPackages = await checkUpdates.run({
+            packageFile: path.join(configPackage.path, 'package.json'),
+            silent: true,
+            jsonUpgraded: true,
+            upgrade: true,
+        });
+
+        if (Object.keys(updatedPackages).length > 0) {
+            console.log(`\n\t'${configPackage.name}' dependencies updated:\n`);
+            for (const [key, value] of Object.entries(updatedPackages)) {
+                console.log(`\t\t${key}: ${value}`);
+            }
+
+            console.log(`\n\tInstalling the updates ${inThePackage}...`);
+            installCommand(configPackage);
+            console.log(`\tUpdates installed ${inThePackage}...\n`);
+        } else {
+            console.log(`\n\tNo dependencies updates ${inThePackage}.\n`);
+
+            console.log(`\n\tInstalling the dependencies ${inThePackage}...`);
+            installCommand(configPackage);
+            console.log(`\tDependencies installed ${inThePackage}...\n`);
+        }
+    } catch (error) {
+        console.log(`\n\tSomething went wrong.\n`);
+    }
+}
+
+
+const installCommand = (
+    configPackage: Package,
+) => {
+    const install = 'yarn install';
+    execSync(
+        install,
+        {
+            cwd: configPackage.path,
+            stdio: 'ignore',
+        },
+    );
 }
 
 
