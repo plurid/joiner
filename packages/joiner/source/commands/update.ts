c61fddbd84d94480879b3259a2b6754e0e83f6c5
Jan Cavel <caveljan@gmail.com>
2020-03-30 10:52:01 +0300

    joiner > dev: commands > update

--- packages/joiner/source/commands/update.ts
+++ packages/joiner/source/commands/update.ts
@@ -52,7 +52,7 @@ const updateLogic = async (
             silent: true,
             jsonUpgraded: true,
             upgrade: true,
-            dep: 'prod,dev,peer',
+            dep: 'prod, dev, peer',
         });
 
         if (Object.keys(updatedPackages).length > 0) {
