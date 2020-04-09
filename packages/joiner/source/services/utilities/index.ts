1c2920a57c3ba78644619d0bd13cf336693f6535
Jan Cavel <caveljan@gmail.com>
2020-04-09 12:13:55 +0300

    joiner > dev: DevelopmentWatcher, debounce

--- packages/joiner/source/services/utilities/index.ts
+++ packages/joiner/source/services/utilities/index.ts
@@ -5,3 +5,42 @@ import {
 
 
 export const fileExists = async (path: string) => !!(await fs.stat(path).catch(e => false));
+
+
+/**
+ * Source: https://stackoverflow.com/a/57335271
+ *
+ * @param callback Function to be called.
+ * @param wait Debounce time.
+ */
+export function debouncedCallback<A extends any[]>(
+    callback: (...args: A) => void,
+    wait: number,
+) {
+    // track args & timeout handle between calls
+    let argsRef: any;
+    let timeout: any;
+
+    function cleanup() {
+        if (timeout) {
+            clearTimeout(timeout);
+        }
+    }
+
+    return function debouncedCallback(
+        ...args: A
+    ) {
+        // capture latest args
+        argsRef.current = args;
+
+        // clear debounce timer
+        cleanup();
+
+        // start waiting again
+        timeout.current = setTimeout(() => {
+            if(argsRef.current) {
+                callback(...argsRef.current);
+            }
+        }, wait);
+    };
+}
