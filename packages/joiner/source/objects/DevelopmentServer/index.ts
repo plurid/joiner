a2bb5d1a7444a2a03ef99b0e5ba17ae0a4401b46
Jan Cavel <caveljan@gmail.com>
2020-04-09 07:29:34 +0300

    joiner > dev: DevelopmentServer, DevelopmentWatcher

--- packages/joiner/source/objects/DevelopmentServer/index.ts
+++ packages/joiner/source/objects/DevelopmentServer/index.ts
@@ -4,27 +4,36 @@ import {
     ConfigurationFile,
 } from '../../data/interfaces';
 
+import DevelopmentWatcher from '../DevelopmentWatcher';
+
 
 
 class DevelopmentServer {
     private server: http.Server;
     private configuration: ConfigurationFile;
+    private watcher: DevelopmentWatcher;
 
     constructor(
         configuration: ConfigurationFile,
     ) {
         this.configuration = configuration;
+        this.watcher = new DevelopmentWatcher(configuration);
         this.server = http.createServer((req, res) => {
         });
 
         process.addListener('SIGINT', () => {
+            this.watcher.stop();
+
             this.server.close();
         });
     }
 
     start() {
-        this.server.listen(this.configuration.development.serverPort, () => {
-            console.log('Joiner development server started.');
+        const port = this.configuration.development.serverPort;
+        this.server.listen(port, () => {
+            console.log(`\n\tJoiner development server started on port ${port}`);
+
+            this.watcher.start();
         });
     }
 }
