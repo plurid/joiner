08e29338029431faf48b379e4275056b9c0ebbc8
ly3xqhl8g9 <ly3xqhl8g9@plurid.com>
2020-04-09 07:29:34 +0300

    joiner > dev: DevelopmentServer, DevelopmentWatcher

---
+++ packages/joiner/source/objects/DevelopmentWatcher/index.ts
@@ -0,0 +1,26 @@
+import {
+    ConfigurationFile,
+} from '../../data/interfaces';
+
+
+
+class DevelopmentWatcher {
+    private configuration: ConfigurationFile;
+
+    constructor(
+        configuration: ConfigurationFile,
+    ) {
+        this.configuration = configuration;
+    }
+
+    start() {
+
+    }
+
+    stop() {
+
+    }
+}
+
+
+export default DevelopmentWatcher;
