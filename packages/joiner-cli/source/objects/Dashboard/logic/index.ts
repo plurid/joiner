23d8cfc49949e483a0c3c648458be6ce9ebdd34a
ly3xqhl8g9 <ly3xqhl8g9@plurid.com>
2020-11-16 22:43:35 +0200

    joiner-cli > dev: Dashboard

--- packages/joiner-cli/source/objects/Dashboard/logic/index.ts
+++ packages/joiner-cli/source/objects/Dashboard/logic/index.ts
@@ -1,15 +1,27 @@
 // #region module
 const dashboardStatus = async () => {
+    // read configuration file
+    // log the data
     console.log(`status.`);
 }
 
 
 const dashboardStart = async () => {
+    // read configuration file
+
+    // check if server already started
+
+    // start server
     console.log(`start.`);
 }
 
 
 const dashboardStop = async () => {
+    // read configuration file
+
+    // check if server started
+
+    // stop server
     console.log(`stop.`);
 }
 
@@ -17,6 +29,8 @@ const dashboardStop = async () => {
 const dashboardRegister = async (
     options: any,
 ) => {
+    // add options.path to configuration
+
     console.log(`register.`, options);
 }
 
@@ -24,6 +38,8 @@ const dashboardRegister = async (
 const dashboardDeregister = async (
     options: any,
 ) => {
+    // remove options.path to configuration
+
     console.log(`deregister.`, options);
 }
 // #endregion module
