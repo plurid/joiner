acc9399cdfdf86a1127d80e64ac95dc60fcc2615
ly3xqhl8g9 <ly3xqhl8g9@plurid.com>
2021-12-15 11:05:42 +0200

    joiner-cli > dev: objects/Dashboard/server | logs

--- packages/joiner-cli/source/objects/Dashboard/server/shared/kernel/services/state/modules/data/initial/index.ts
+++ packages/joiner-cli/source/objects/Dashboard/server/shared/kernel/services/state/modules/data/initial/index.ts
@@ -10,25 +10,7 @@
 const initialState: Types.State = {
     id: '',
     configurations: [],
-    logs: [
-        {
-            id: 'one',
-            package: 'name',
-            command: 'run',
-            startAt: 0,
-            finishedAt: 0,
-            data: [
-                'started at 12/15/2021, 10:49:55 AM',
-                '',
-                '',
-                '    Patching version for package global...',
-                '    global version patched to version 0.0.1.',
-                '',
-                '',
-                'finished at 12/15/2021, 10:49:56 AM',
-            ],
-        },
-    ],
+    logs: [],
 };
 // #endregion module
 
