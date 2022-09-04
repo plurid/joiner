e93091b73b2112cc6695696bb03de04a28f3b726
ly3xqhl8g9 <ly3xqhl8g9@plurid.com>
2022-09-04 14:59:46 +0300

    joiner-cli > dev: objects/Dashboard/server

--- packages/joiner-cli/source/objects/Dashboard/server/shared/routes/index.tsx
+++ packages/joiner-cli/source/objects/Dashboard/server/shared/routes/index.tsx
@@ -63,26 +63,11 @@ const notFoundRoute: PluridReactRoute = {
             title="not found · joiner"
         />
     ),
-    spaces: [
-        {
-            value: 'default',
-            universes: [
-                {
-                    value: 'default',
-                    clusters: [
-                        {
-                            value: 'default',
-                            planes: [
-                                {
-                                    value: '/',
-                                    component: NotFoundPlane,
-                                },
-                            ],
-                        },
-                    ],
-                },
-            ],
-        },
+    planes: [
+        [ '/not-found', NotFoundPlane ],
+    ],
+    view: [
+        '/not-found',
     ],
 };
 
